bc2812e9a3d85715f574708075d3033d7454208c
sridharpedamallu <sridharpedamallu@gmail.com>
2020-03-21 22:30:30 +0530

    nav

--- pas-app/src/app/app-routing.module.ts
+++ pas-app/src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { VendorComponent } from './components/vendor/vendor.component';
+import { ItemMasterComponent } from './components/item-master/item-master.component';
+import { StockLineComponent } from './components/stock-line/stock-line.component';
 
 
 const routes: Routes = [
   {path: '', component: DashboardComponent},
-  {path: 'customer', component: CustomerComponent}
+  {path: 'customer', component: CustomerComponent},
+  {path: 'employee', component: EmployeeComponent},
+  {path: 'vendor', component: VendorComponent},
+  {path: 'item-master', component: ItemMasterComponent},
+  {path: 'stock-line', component: StockLineComponent},
 ];
 
 @NgModule({
